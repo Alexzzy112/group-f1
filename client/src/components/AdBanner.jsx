1c3ee0db94b377de7d439ac03717a48a2c969094
Alexzzy Silas <azamukwokusilas@gmail.com>
2026-05-30 23:20:25 +0100

    Update Adsterra ad placements

--- client/src/components/AdBanner.jsx
+++ client/src/components/AdBanner.jsx
@@ -1,7 +1,7 @@
 export default function AdBanner({ className = '' }) {
   return (
     <div className={`flex justify-center my-6 ${className}`}>
-      <div id="container-4415b89afaabc02957559221d9456b4b" />
+      <div id="container-9410f38442339753136574041559536d" />
     </div>
   );
 }
