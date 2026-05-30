6a91c3f347a4ec97f3613817233314625214f1f2
Alexzzy Silas <azamukwokusilas@gmail.com>
2026-05-30 23:02:08 +0100

    Remove Adsterra ads

--- client/src/components/AdBanner.jsx
+++
@@ -1,7 +0,0 @@
-export default function AdBanner({ className = '' }) {
-  return (
-    <div className={`flex justify-center my-6 ${className}`}>
-      <div id="container-4415b89afaabc02957559221d9456b4b" />
-    </div>
-  );
-}
