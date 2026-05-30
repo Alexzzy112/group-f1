99d9e4fda9b8655476072e656884d7ed59eb154c
Alexzzy Silas <azamukwokusilas@gmail.com>
2026-05-30 22:46:12 +0100

    Add Adsterra native banner ads

---
+++ client/src/components/AdBanner.jsx
@@ -0,0 +1,7 @@
+export default function AdBanner({ className = '' }) {
+  return (
+    <div className={`flex justify-center my-6 ${className}`}>
+      <div id="container-ee42e0b9379c023864569a1130573fe0" />
+    </div>
+  );
+}
